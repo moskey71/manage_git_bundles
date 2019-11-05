948f64c66d6638a833e072b253687f886ad15e6c
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2019-11-05 23:59:21 +0000

    rework deltas

--- _update-repos.sh
+++ _update-repos.sh
@@ -4,14 +4,14 @@ STARTDIR=$(pwd)
 
 if [ -d $STARTDIR/_bundles ]
   then
-    echo "_bundles folder already exists.  skipping folder creation"
+    echo "_bundles folder already exists.  Skipping folder creation"
   else
     echo "Creating _bundles folder..."
     mkdir $STARTDIR/_bundles
   fi
 
 clone_repo(){
-git clone $1
+git clone $1 --mirror
 }
 
 
@@ -20,31 +20,33 @@ while IFS= read -r line
 do
 
   REPO=$(basename $line .git)
-  REPOFOLDER=$STARTDIR/$REPO
+  REPOFOLDER=$STARTDIR/$REPO.git
   echo "repofolder is" $REPOFOLDER
   if [ -d $REPOFOLDER ]
   then
-    echo "$REPO already exists. Skipping folder creation..."
+    echo "$REPO already exists. Updating $REPO to latest..."
+    git -C $REPOFOLDER fetch --all --prune
   else
     echo "Cloning $REPO"
-    clone_repo $line
+    clone_repo $line --mirror
 fi
 done < "$input"
 
 update_repos(){
-echo "Entering $1 directory..."
-cd $STARTDIR/$1
-echo "Fetching updates..."
-git fetch --all
-echo "Exporting git bundle to $STARTDIR/_bundles/$1.bundle..."
-git bundle create $STARTDIR/_bundles/$1.bundle --all
+# echo "Entering $1 directory..."
+# cd $STARTDIR/$1
+# echo "Fetching updates..."
+# git -C $STARTDIR/$1 fetch --all
+REPO=$( echo $1 | sed 's/\.git//g')
+echo "Exporting git bundle to $STARTDIR/_bundles/$REPO.bundle..."
+git -C $STARTDIR/$1 bundle create $STARTDIR/_bundles/$REPO.bundle --tags --branches --since=10.days
 }
 
 dirs=$(ls -d */ -1 | sed 's/\///' | sed 's/\<_bundles\>//')
 
 for i in $dirs
 do
-  echo "Updating $i repo..."
+  echo "Creating bundle from $i repo..."
   update_repos $i
 done
 
